c8e24e4de77fe76ac2e152c900ad71a708b6b3a8
enderman859 <juana.henaoa@autonoma.edu.co>
2025-02-05 22:13:34 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
- import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import JSConfetti from 'js-confetti';
 function App() {
   const jsConfetti = new JSConfetti();
